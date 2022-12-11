6c2fc00d6a3fdb0b5fd4e379feef598f016ba917
sonamshrestha <xresthasonam@gmail.com>
2022-12-11 17:00:53 +0545

    Basics Java web socket

--- socket/src/App.js
+++ socket/src/App.js
@@ -5,8 +5,8 @@ import SockJsClient from 'react-stomp';
 const SOCKET_URL = 'http://localhost:8082/ws-message';
 
 const App = () => {
-  const [revieved, setRecieved] = useState('You server message here.');
-
+  const [revieved, setRecieved] = useState([]);
+  // const sockJs = new SockJS('', null, {});
   const [message, setMessage] = useState('');
 
   let onConnected = () => {
@@ -15,7 +15,10 @@ const App = () => {
 
   let onMessageReceived = (msg) => {
     console.log('message', msg)
-    setRecieved(msg.message);
+    let mesg = setRecieved((e) => [...e, msg.message]);
+    setMessage('')
+    // mesg.push(msg.message)
+    // setRecieved(mesg);
   }
   const send = () => {
     axios.post('http://localhost:8082/web/send', { message: message }, {}).then((res) => {
@@ -35,10 +38,151 @@ const App = () => {
         onMessage={msg => onMessageReceived(msg)}
         debug={false}
       />
-      <div>{revieved}</div>
+      <div>{revieved.map((e) => {
+        return (<li>{e}</li>)
+      })}</div>
 
-      <input type="text" onChange={e => setMessage(e.target.value)} value={message} />
-      <button onClick={send} >Send</button>
+      {/* <input type="text" /> */}
+
+
+      <div class="menu">
+        <div class="back"><i class="fa fa-chevron-left"></i> <img src="https://i.imgur.com/DY6gND0.png" draggable="false" /></div>
+        <div class="name">Alex</div>
+        <div class="last">18:09</div>
+      </div>
+      <ol class="chat">
+        <li class="other">
+          <div class="avatar"><img src="https://i.imgur.com/DY6gND0.png" draggable="false" /></div>
+          <div class="msg">
+            <p>Hola!</p>
+            <p>Te vienes a cenar al centro? <emoji class="pizza" /></p>
+            <time>20:17</time>
+          </div>
+        </li>
+        <li class="self">
+          <div class="avatar"><img src="https://i.imgur.com/HYcn9xO.png" draggable="false" /></div>
+          <div class="msg">
+            <p>Puff...</p>
+            <p>Aún estoy haciendo el contexto de Góngora... <emoji class="books" /></p>
+            <p>Mejor otro día</p>
+            <time>20:18</time>
+          </div>
+        </li>
+        <li class="other">
+          <div class="avatar"><img src="https://i.imgur.com/DY6gND0.png" draggable="false" /></div>
+          <div class="msg">
+            <p>Qué contexto de Góngora? <emoji class="suffocated" /></p>
+            <time>20:18</time>
+          </div>
+        </li>
+        <li class="self">
+          <div class="avatar"><img src="https://i.imgur.com/HYcn9xO.png" draggable="false" /></div>
+          <div class="msg">
+            <p>El que mandó Marialu</p>
+            <p>Es para mañana...</p>
+            <time>20:18</time>
+          </div>
+        </li>
+        <li class="other">
+          <div class="avatar"><img src="https://i.imgur.com/DY6gND0.png" draggable="false" /></div>
+          <div class="msg">
+            <p><emoji class="scream" /></p>
+            <p>Pásamelo! <emoji class="please" /></p>
+            <time>20:18</time>
+          </div>
+        </li>
+        <li class="self">
+          <div class="avatar"><img src="https://i.imgur.com/HYcn9xO.png" draggable="false" /></div>
+          <div class="msg">
+            <img src="https://i.imgur.com/QAROObc.jpg" draggable="false" />
+            <time>20:19</time>
+          </div>
+        </li>
+        <li class="other">
+          <div class="avatar"><img src="https://i.imgur.com/DY6gND0.png" draggable="false" /></div>
+          <div class="msg">
+            <p>Gracias! <emoji class="hearth_blue" /></p>
+            <time>20:20</time>
+          </div>
+        </li>
+        <div class="day">Hoy</div>
+        <li class="self">
+          <div class="avatar"><img src="https://i.imgur.com/HYcn9xO.png" draggable="false" /></div>
+          <div class="msg">
+            <p>Te apetece jugar a Minecraft?</p>
+            <time>18:03</time>
+          </div>
+        </li>
+        <li class="other">
+          <div class="avatar"><img src="https://i.imgur.com/DY6gND0.png" draggable="false" /></div>
+          <div class="msg">
+            <p>Venga va, hace ya mucho que no juego...</p>
+            <time>18:07</time>
+          </div>
+        </li>
+        <li class="self">
+          <div class="avatar"><img src="https://i.imgur.com/HYcn9xO.png" draggable="false" /></div>
+          <div class="msg">
+            <p>Ehh, me crashea el Launcher... <emoji class="cryalot" /></p>
+            <time>18:08</time>
+          </div>
+        </li>
+        <li class="other">
+          <div class="avatar"><img src="https://i.imgur.com/DY6gND0.png" draggable="false" /></div>
+          <div class="msg">
+            <p><emoji class="lmao" /></p>
+            <time>18:08</time>
+          </div>
+        </li>
+        <li class="self">
+          <div class="avatar"><img src="https://i.imgur.com/HYcn9xO.png" draggable="false" /></div>
+          <div class="msg">
+            <p>Es broma</p>
+            <p>Ataque Moai!</p>
+            <p><span><emoji class="moai" /></span><span><emoji class="moai" /></span><span><emoji class="moai" /></span><span><emoji class="moai" /></span><span><emoji class="moai" /></span><span><emoji class="moai" /></span></p>
+            <time>18:09</time>
+          </div>
+        </li>
+        <li class="other">
+          <div class="avatar"><img src="https://i.imgur.com/DY6gND0.png" draggable="false" /></div>
+          <div class="msg">
+            <p>Copón</p>
+            <p><emoji class="funny" /></p>
+            <time>18:08</time>
+          </div>
+        </li>
+        <li class="self">
+          <div class="avatar"><img src="https://i.imgur.com/HYcn9xO.png" draggable="false" /></div>
+          <div class="msg">
+            <p>Hey there's a new update about this chat UI with more responsive elements! Check it now:</p>
+            <p><a href="https://codepen.io/Varo/pen/YPmwpQ" target="parent">Chat UI 2.0</a></p>
+            <time>18:09</time>
+          </div>
+        </li>
+
+        {revieved.map(e => {
+          return (
+            <li class="self">
+              <div class="avatar"><img src="https://i.imgur.com/HYcn9xO.png" draggable="false" /></div>
+              <div class="msg">
+                <p>{e}</p>
+                <p><a href="https://codepen.io/Varo/pen/YPmwpQ" target="parent">Chat UI 2.0</a></p>
+                <time>18:09</time>
+              </div>
+            </li>
+          )
+        })}
+      </ol>
+      <input class="textarea" onChange={e => setMessage(e.target.value)} value={message} type="text" placeholder="Type here!" />
+      <div class="emojis">
+
+        <button onClick={send} >Send</button>
+
+      </div>
+      <div>
+
+
+      </div>
     </div>
   );
 }
